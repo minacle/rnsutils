51b0de0cf900151d0e79c57480d20e42ab120987
Olivier Jolly <olivier@pcedev.com>
2016-02-02 16:14:46 +0100

    Prepare for the 0.8.0 release
    Creates change logs along with updating the setup scripts.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.7.0',
+    version='0.8.0',
 
     description='Renoise library and utilities',
     long_description=long_description,
@@ -87,7 +87,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['lxml', 'sf2utils>=0.7.0'],
+    install_requires=['lxml', 'sf2utils>=0.8.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
